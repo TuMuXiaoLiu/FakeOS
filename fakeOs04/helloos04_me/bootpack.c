9d731f9cf2d1d3b4d4cf720776f5c9446a33d59c
chunkingcao <chunlincao@foxmail.com>
2023-09-06 17:42:56 +0800

    add fakeOs04

---
+++ fakeOs04/helloos04_me/bootpack.c
@@ -0,0 +1,144 @@
+/*告诉C编译器，有一个函数在别的文件里 自己找一下
+函数声明不用{}，只用;意为 函数在别的文件中，需要找一下
+即便是写在同一个文件中 也需要在此处声明*/
+void io_hlt(void);
+void io_cli(void);
+void io_out8(int port, int data);
+int io_load_eflags(void);
+void io_store_eflags(int eflags);
+//设置颜色
+void init_palette(void);
+void set_palette(int start, int end, unsigned char *rgb);
+//画矩形
+void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
+//void write_mem8(int addr, int data);
+
+#define COL8_000000		0
+#define COL8_FF0000		1
+#define COL8_00FF00		2
+#define COL8_FFFF00		3
+#define COL8_0000FF		4
+#define COL8_FF00FF		5
+#define COL8_00FFFF		6
+#define COL8_FFFFFF		7
+#define COL8_C6C6C6		8
+#define COL8_840000		9
+#define COL8_008400		10
+#define COL8_848400		11
+#define COL8_000084		12
+#define COL8_840084		13
+#define COL8_008484		14
+#define COL8_848484		15
+
+void HariMain(void){
+
+	/*声明一个变量i i是32位整数*/
+	int i;	
+	//变量P，使用Byte类型地址
+	//char *p;
+
+	//设定调色板
+	init_palette();
+
+	//将地址赋值进去
+	//char *p = (char *)0xa0000;
+
+	/*使用下方的C语言替代
+	for(i = 0xa0000;i <= 0xaffff; i++){
+		//等效于 MOV BYTE[i],15
+		//write_mem8(i,15);
+		//让屏幕出现带颜色的竖条纹
+		//write_mem8(i,i&0x0f);
+	}
+	for(i = 0xa0000;i <= 0xaffff;i++){
+		//代入地址
+		//p = i;
+		//*p=i & 0x0f;
+		*((char *)i) = i & 0x0f;
+	}*/
+	
+	/*画三个矩形
+	//将地址赋值进去
+	//char *p = (char *)0xa0000;
+	boxfill8(p, 320, COL8_FF0000, 20, 20, 120, 120);
+	boxfill8(p, 320, COL8_00FF00, 70, 50, 170, 150);
+	boxfill8(p, 320, COL8_0000FF, 120, 80, 220, 180);
+	*/
+
+	//画 初始界面
+	char *vram = (char *) 0xa0000;/* 地址变量赋值 */
+	int xsize = 320;
+	int ysize = 200;
+	/* 根据 0xa0000 + x + y * 320 计算坐标 8*/
+	boxfill8(vram, xsize, COL8_008484,  0,         0,          xsize -  1, ysize - 29);
+	boxfill8(vram, xsize, COL8_C6C6C6,  0,         ysize - 28, xsize -  1, ysize - 28);
+	boxfill8(vram, xsize, COL8_FFFFFF,  0,         ysize - 27, xsize -  1, ysize - 27);
+	boxfill8(vram, xsize, COL8_C6C6C6,  0,         ysize - 26, xsize -  1, ysize -  1);
+
+	boxfill8(vram, xsize, COL8_FFFFFF,  3,         ysize - 24, 59,         ysize - 24);
+	boxfill8(vram, xsize, COL8_FFFFFF,  2,         ysize - 24,  2,         ysize -  4);
+	boxfill8(vram, xsize, COL8_848484,  3,         ysize -  4, 59,         ysize -  4);
+	boxfill8(vram, xsize, COL8_848484, 59,         ysize - 23, 59,         ysize -  5);
+	boxfill8(vram, xsize, COL8_000000,  2,         ysize -  3, 59,         ysize -  3);
+	boxfill8(vram, xsize, COL8_000000, 60,         ysize - 24, 60,         ysize -  3);
+
+	boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 24, xsize -  4, ysize - 24);
+	boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 23, xsize - 47, ysize -  4);
+	boxfill8(vram, xsize, COL8_FFFFFF, xsize - 47, ysize -  3, xsize -  4, ysize -  3);
+	boxfill8(vram, xsize, COL8_FFFFFF, xsize -  3, ysize - 24, xsize -  3, ysize -  3);
+
+	for(;;){
+		io_hlt();	
+	}
+}
+
+void init_palette(void){
+	//C语言中的static char 语句只能用于数据，相当于汇编中的DB指令
+	static unsigned char table_rgb[16 * 3] = {
+		0x00, 0x00, 0x00,	/*  0:黑 */
+		0xff, 0x00, 0x00,	/*  1:梁红 */
+		0x00, 0xff, 0x00,	/*  2:亮绿 */
+		0xff, 0xff, 0x00,	/*  3:亮黄 */
+		0x00, 0x00, 0xff,	/*  4:亮蓝 */
+		0xff, 0x00, 0xff,	/*  5:亮紫 */
+		0x00, 0xff, 0xff,	/*  6:浅亮蓝 */
+		0xff, 0xff, 0xff,	/*  7:白 */
+		0xc6, 0xc6, 0xc6,	/*  8:亮灰 */
+		0x84, 0x00, 0x00,	/*  9:暗红 */
+		0x00, 0x84, 0x00,	/* 10:暗绿 */
+		0x84, 0x84, 0x00,	/* 11:暗黄 */
+		0x00, 0x00, 0x84,	/* 12:暗青 */
+		0x84, 0x00, 0x84,	/* 13:暗紫 */
+		0x00, 0x84, 0x84,	/* 14:浅暗蓝 */
+		0x84, 0x84, 0x84	/* 15:暗灰 */
+	};
+	set_palette(0, 15, table_rgb);
+}
+
+void set_palette(int start, int end, unsigned char *rgb){
+	int i, eflags;
+	//记录中断许可标识的值
+	eflags = io_load_eflags();
+	//将中断许可标识置为0，禁止中断
+	io_cli();
+	io_out8(0x03c8, start);
+	for(i = start;i <= end; i++){
+		io_out8(0x03c9, rgb[0] / 4);
+		io_out8(0x03c9, rgb[1] / 4);
+		io_out8(0x03c9, rgb[2] / 4);
+		rgb += 3;
+	}
+	//复原中断许可标识
+	io_store_eflags(eflags);
+}
+
+void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1){
+	int x, y;
+	for (y = y0;y <= y1; y++)
+	{
+		for (x = x0; x <= x1; x++)
+		{
+			vram[y * xsize + x] = c;
+		}
+	}
+}
