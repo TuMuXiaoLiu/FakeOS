4cd0dd59008febacf39af2ea8b99ba5cbede28c8
chunkingcao <chunlincao@foxmail.com>
2023-09-20 14:46:25 +0800

    add dir fakeOs17

---
+++ fakeOs17/helloos17_me/bootpack3.0.c
@@ -0,0 +1,433 @@
+//要使用sprintg函数 必须在开头加上 #include
+
+//添加函数引用文件
+#include "bootpack.h"
+#include <stdio.h>
+
+void make_window8(unsigned char *buf, int xsize, int ysize, char *title, char act);
+void make_wtitle8(unsigned char *buf, int xsize, char *title, char act);
+void putfonts8_asc_sht(struct SHEET *sht, int x, int y, int c, int b, char *s, int l);
+void make_textbox8(struct SHEET *sht, int x0, int y0, int sx, int sy, int c);
+void console_task(struct SHEET *sheet);
+
+
+//程序入口 相当于Java中的Main函数
+void HariMain(void){
+	//声明BOOTINFO的对象并赋值 指针赋值
+	struct BOOTINFO *binfo = (struct BOOTINFO *)ADR_BOOTINFO;
+	//声明缓冲流
+	struct FIFO32 fifo;
+	//用于存放输出内容
+	char s[40];
+	//缓冲buff数组
+	int fifobuf[128];
+	//鼠标坐标
+	int mx, my, i, cursor_x, cursor_c;
+	//声明内存总数
+	unsigned int memtotal;
+	struct MOUSE_DEC mdec;
+	//声明内存管理对象
+	struct MEMMAN *memman = (struct MEMMAN *)MEMMAN_ADDR;
+
+	//图层管理
+	struct SHTCTL *shtctl;
+	//定义键盘输入
+	static	char keytable[0x54] = {
+		0, 0, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '^', 0, 0,
+		'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '@', '[', 0, 0, 'A', 'S',
+		'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', ':', 0, 0, ']', 'Z', 'X', 'C', 'V',
+		'B', 'N', 'M', ',', '.', '/', 0, '*', 0, ' ', 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, '7', '8', '9', '-', '4', '5', '6', '+', '1',
+		'2', '3', '0', '.'
+	};
+	int key_to = 0;
+
+	//背景板  鼠标   窗口
+	struct SHEET *sht_back, *sht_mouse, *sht_win, *sht_cons;
+	//背景板缓冲  鼠标缓冲
+	unsigned char *buf_back, buf_mouse[256], *buf_win, *buf_cons;
+	//声明多任务对象
+	struct TASK *task_a, *task_cons;
+	//声明计时器对象
+	struct TIMER *timer;
+	
+
+	//初始化GDTIDT
+	init_gdtidt();
+	//初始化PIC
+	init_pic();
+	//解除 禁止中断 在IDT初始化时所有中断都会被禁止，需要在此解除所有的禁止中断 让CPU接受外部设备指令
+	io_sti();
+
+	//初始化缓冲区
+	fifo32_init(&fifo, 128, fifobuf, 0);
+	//初始化定时器
+	init_pit();
+	//初始化鼠标键盘
+	init_keyboard(&fifo, 256);
+	enable_mouse(&fifo, 512, &mdec);
+
+	//修改PIT和PIC1和键盘的中断，许可为11111000
+	io_out8(PIC0_IMR, 0xf8);
+	//修改PIC的IMR，以便接受来自鼠标的中断
+	io_out8(PIC1_IMR, 0xef); /* 許可(11101111) */
+	
+	//进行内存管理
+	memtotal = memtest( 0x00400000, 0xbfffffff);
+	memman_init(memman);
+	// 0x00001000 - 0x0009efff
+	memman_free(memman, 0x00001000, 0x0009e000);
+	memman_free(memman, 0x00400000, memtotal - 0x00400000);
+
+	//设定调色板
+	init_palette();
+	//调用画界面方法并入参-> 启动图层管理
+	shtctl = shtctl_init(memman, binfo->vram, binfo->scrnx, binfo->scrny);
+	
+	task_a = task_init(memman);
+	fifo.task = task_a;
+	task_run(task_a, 1, 0);
+	
+
+	//sht_back
+	//获取背景板图层对象
+	sht_back = sheet_alloc(shtctl);
+	//背景板缓冲
+	buf_back = (unsigned char *)memman_alloc_4k(memman, binfo->scrnx * binfo->scrny);
+	//背景板没有透明色
+	sheet_setbuf(sht_back, buf_back, binfo->scrnx, binfo->scrny, -1);
+	//初始化屏幕
+	init_screen8(buf_back, binfo->scrnx, binfo->scrny);
+
+
+	//sht_cons
+	sht_cons = sheet_alloc(shtctl);
+	buf_cons = (unsigned char *)memman_alloc_4k(memman, 256 * 165);
+	sheet_setbuf(sht_cons, buf_cons, 256, 165, -1);//无透明色
+	make_window8(buf_cons, 256, 165, "console", 0);
+	make_textbox8(sht_cons, 8, 28, 240, 128, COL8_000000);
+	task_cons = task_alloc();
+	task_cons->tss.esp = memman_alloc_4k(memman, 64 * 1024) + 64 * 1024 -8;
+	task_cons->tss.eip = (int)&console_task;
+	task_cons->tss.es = 1 * 8;
+	task_cons->tss.cs = 2 * 8;
+	task_cons->tss.ss = 1 * 8;
+	task_cons->tss.ds = 1 * 8;
+	task_cons->tss.fs = 1 * 8;
+	task_cons->tss.gs = 1 * 8;
+	*((int *)(task_cons->tss.esp + 4)) = (int)sht_cons;
+	//level 2, priority 2
+	task_run(task_cons, 2, 2);
+
+	//sht_win
+	//获取窗口图层对象
+	sht_win = sheet_alloc(shtctl);
+	//窗口缓冲
+	buf_win = (unsigned char *)memman_alloc_4k(memman, 160 * 52);
+	//窗口
+	sheet_setbuf(sht_win, buf_win, 144, 52, -1);
+	//调用窗口函数
+	make_window8(buf_win, 144, 52, "task_a", 1);
+	make_textbox8(sht_win, 8, 28, 128, 16, COL8_FFFFFF);
+	cursor_x = 8;
+	cursor_c = COL8_FFFFFF;
+	//初始化光标计时器
+	timer = timer_alloc();
+	timer_init(timer, &fifo, 1);
+	timer_settime(timer, 50);
+
+
+	//sht_mouse
+	//获取鼠标图层对象
+	sht_mouse = sheet_alloc(shtctl);
+	//鼠标的透明色号99
+	sheet_setbuf(sht_mouse, buf_mouse, 16, 16, 99);
+	//初始化鼠标  背景色号99 
+	init_mouse_cursor8(buf_mouse, 99);
+	//计算画面的中心坐标
+	mx = (binfo->scrnx - 16) / 2; 
+	my = (binfo->scrny - 28 - 16) / 2;
+
+
+	//背景板位置
+	sheet_slide(sht_back,  0,  0);
+	//显示窗口
+	sheet_slide(sht_cons, 32,  4);
+	sheet_slide(sht_win,  64, 56);
+	//显示并勾勒鼠标
+	sheet_slide(sht_mouse, mx, my);	
+
+	//设置背景板图层高度
+	sheet_updown(sht_back,	0);
+	//设置窗口图层高度
+	sheet_updown(sht_cons,	1);
+	sheet_updown(sht_win,	2);
+	//设置鼠标图层高度
+	sheet_updown(sht_mouse, 3);
+
+	//打印鼠标坐标
+	sprintf(s, "(%3d, %3d)", mx, my);
+	putfonts8_asc_sht(sht_back, 0, 0, COL8_FFFFFF, COL8_008484, s, 10);
+	//打印 内存大小以及空余空间
+	sprintf(s, "memory %dMB  free : %dKB", memtotal / (1024*1024), memman_total(memman) / 1024);
+	putfonts8_asc_sht(sht_back, 0, 32, COL8_FFFFFF, COL8_008484, s, 40);
+
+
+	for (;;) {
+		io_cli();
+		if (fifo32_status(&fifo) == 0) {
+			task_sleep(task_a);
+			io_sti();
+		} else {
+			i = fifo32_get(&fifo);
+			io_sti();
+			if (256 <= i && i <= 511){	//键盘
+				sprintf(s, "%02X", i - 256);
+				putfonts8_asc_sht(sht_back, 0, 16, COL8_FFFFFF, COL8_008484, s, 2);
+				if (i < 0x54 + 256){
+					if (keytable[i - 256] != 0 && cursor_x < 128){
+						//显示一个字符就前移一次光标
+						s[0] = keytable[i - 256];
+						s[1] = 0;
+						putfonts8_asc_sht(sht_win, cursor_x, 28, COL8_000000, COL8_FFFFFF, s, 1);
+						cursor_x += 8;
+					}
+				}
+				//退格键
+				if (i == 256 + 0x0e && cursor_x > 8){
+					//用空格键把光标消除后， 后移一位光标
+					putfonts8_asc_sht(sht_win, cursor_x, 28, COL8_000000, COL8_FFFFFF, " ", 1);
+					cursor_x -= 8;
+				}
+				//TAB键
+				if (i == 256 + 0x0f){
+					if (key_to == 0){
+						key_to = 1;
+						make_wtitle8(buf_win, sht_win->bxsize, "task_a", 0);
+						make_wtitle8(buf_cons, sht_cons->bxsize, "console", 1);
+					} else {
+						key_to = 0;
+						make_wtitle8(buf_win, sht_win->bxsize, "task_a", 1);
+						make_wtitle8(buf_cons, sht_cons->bxsize, "console", 0);
+					}
+					sheet_refresh(sht_win, 0, 0, sht_win->bxsize, 21);
+					sheet_refresh(sht_cons, 0, 0, sht_cons->bxsize, 21);
+				}
+				//光标再显示
+				boxfill8(sht_win->buf, sht_win->bxsize, cursor_c, cursor_x, 28, cursor_x +7, 43);
+				sheet_refresh(sht_win, cursor_x, 28, cursor_x + 8, 44);
+			} else if (512 <= i && i <= 767) {//鼠标
+				//鼠标的三个字节数据都齐了，显示出来
+				if (mouse_decode(&mdec, i - 512) != 0) {
+					//显示鼠标移动数据
+					sprintf(s, "[lcr %4d %4d]", mdec.x, mdec.y);
+					if ((mdec.btn & 0x01) != 0) { s[1] = 'L'; }
+					if ((mdec.btn & 0x02) != 0) { s[3] = 'R'; }
+					if ((mdec.btn & 0x04) != 0) { s[2] = 'C'; }
+					putfonts8_asc_sht(sht_back, 32, 16, COL8_FFFFFF, COL8_008484, s, 15);
+
+					mx += mdec.x;
+					my += mdec.y;
+					//控制鼠标的边界范围
+					if (mx < 0) { mx = 0; }
+					if (my < 0) { my = 0; }
+
+					if (mx > binfo->scrnx -1) { mx = binfo->scrnx -1; }
+					if (my > binfo->scrny -1) { my = binfo->scrny -1; }
+
+					sprintf(s, "(%3d, %3d)", mx, my);
+					putfonts8_asc_sht(sht_back, 0, 0, COL8_FFFFFF, COL8_008484, s, 10);
+					//刷新鼠标指针
+					sheet_slide(sht_mouse, mx, my);
+					//鼠标拖动窗口
+					if (mdec.btn & 0x01 != 0){
+						sheet_slide(sht_win, mx-80, my - 8);
+					}
+				}
+			} else if (i <= 1){//光标定时器
+				if (i !=0 ){
+					timer_init(timer, &fifo, 0);
+					cursor_c = COL8_000000;
+				} else {//光标定时器
+					timer_init(timer, &fifo, 1);
+					cursor_c = COL8_FFFFFF;
+				}
+				timer_settime(timer, 50);
+				boxfill8(sht_win->buf, sht_win->bxsize, cursor_c, cursor_x, 28, cursor_x + 7, 43);
+				sheet_refresh(sht_win, cursor_x, 28, cursor_x + 8, 44);
+			}
+		}
+	}
+}
+
+
+//创建窗口的函数
+void make_window8(unsigned char *buf, int xsize, int ysize, char *title, char act){
+	boxfill8(buf, xsize, COL8_C6C6C6,		  0,	  	 0, xsize - 1,		   0);
+	boxfill8(buf, xsize, COL8_FFFFFF,		  1,		 1, xsize - 2,	 	   1);
+	boxfill8(buf, xsize, COL8_C6C6C6,		  0,		 0,		    0, ysize - 1);
+	boxfill8(buf, xsize, COL8_FFFFFF,		  1,		 1,		    1, ysize - 2);
+	boxfill8(buf, xsize, COL8_848484, xsize - 2,		 1, xsize - 2, ysize - 2);
+	boxfill8(buf, xsize, COL8_000000, xsize - 1,		 0, xsize - 1, ysize - 1);
+	boxfill8(buf, xsize, COL8_C6C6C6,		  2,		 2, xsize - 3, ysize - 3);
+	boxfill8(buf, xsize, COL8_848484,		  1, ysize - 2, xsize - 2, ysize - 2);
+	boxfill8(buf, xsize, COL8_000000,		  0, ysize - 1, xsize - 1, ysize - 1);
+	make_wtitle8(buf, xsize, title, act);
+	return;
+}
+
+//标题函数
+void make_wtitle8(unsigned char *buf, int xsize, char *title, char act){
+	static char closebtn[14][16] = {
+		"OOOOOOOOOOOOOOO@",
+		"OQQQQQQQQQQQQQ$@",
+		"OQQQQQQQQQQQQQ$@",
+		"OQQQ@@QQQQ@@QQ$@",
+		"OQQQQ@@QQ@@QQQ$@",
+		"OQQQQQ@@@@QQQQ$@",
+		"OQQQQQQ@@QQQQQ$@",
+		"OQQQQQ@@@@QQQQ$@",
+		"OQQQQ@@QQ@@QQQ$@",
+		"OQQQ@@QQQQ@@QQ$@",
+		"OQQQQQQQQQQQQQ$@",
+		"OQQQQQQQQQQQQQ$@",
+		"O$$$$$$$$$$$$$$@",
+		"@@@@@@@@@@@@@@@@"
+	};
+
+	int x, y;
+	char c, tc, tbc;
+	if (act != 0){
+		tc = COL8_FFFFFF;
+		tbc = COL8_000084;
+	} else {
+		tc = COL8_C6C6C6;
+		tbc = COL8_848484;
+	}
+	boxfill8(buf, xsize, tbc, 3, 3, xsize - 4, 20);
+	putfonts8_asc(buf, xsize, 24, 4, tc, title);
+
+	for (y = 0; y < 14; y++) {
+		for (x = 0; x < 16; x++) {
+			c = closebtn[y][x];
+			if (c == '@') {
+				c = COL8_000000;
+			} else if (c == '$') {
+				c = COL8_848484;
+			} else if (c == 'Q') {
+				c = COL8_C6C6C6;
+			} else {
+				c = COL8_FFFFFF;
+			}
+			buf[(5 + y) * xsize + (xsize - 21 + x)] = c;
+		}
+	}
+	return;
+}
+
+//重写输入函数  xy 显示位置的坐标  c  字符颜色(color)  b  背景颜色(back color)  s  字符串(string)  l  字符串长度(length)
+void putfonts8_asc_sht(struct SHEET *sht, int x, int y, int c, int b, char *s, int l){
+	boxfill8(sht->buf, sht->bxsize, b, x, y, x + l * 8 - 1, y + 15);
+	putfonts8_asc(sht->buf, sht->bxsize, x, y, c, s);
+	sheet_refresh(sht, x, y, x + l * 8, y + 16);
+	return;
+}
+
+void make_textbox8(struct SHEET *sht, int x0, int y0, int sx, int sy, int c){
+	int x1 = x0 + sx, y1 = y0 + sy;
+	boxfill8(sht->buf, sht->bxsize, COL8_848484, x0 - 2, y0 - 3, x1 + 1, y0 - 3);
+	boxfill8(sht->buf, sht->bxsize, COL8_848484, x0 - 3, y0 - 3, x0 - 3, y1 + 1);
+	boxfill8(sht->buf, sht->bxsize, COL8_FFFFFF, x0 - 3, y1 + 2, x1 + 1, y1 + 2);
+	boxfill8(sht->buf, sht->bxsize, COL8_FFFFFF, x1 + 2, y0 - 3, x1 + 2, y1 + 2);
+	boxfill8(sht->buf, sht->bxsize, COL8_000000, x0 - 1, y0 - 2, x1 + 0, y0 - 2);
+	boxfill8(sht->buf, sht->bxsize, COL8_000000, x0 - 2, y0 - 2, x0 - 2, y1 + 0);
+	boxfill8(sht->buf, sht->bxsize, COL8_C6C6C6, x0 - 2, y1 + 1, x1 + 0, y1 + 1);
+	boxfill8(sht->buf, sht->bxsize, COL8_C6C6C6, x1 + 1, y0 - 2, x1 + 1, y1 + 1);
+	boxfill8(sht->buf, sht->bxsize, c,			 x0 - 1, y0 - 1, x1 + 0, y1 + 0);
+	return;
+}
+
+void task_b_main(struct SHEET *sht_win_b){
+	struct FIFO32 fifo;
+	struct TIMER *timer_1s;
+	int i, fifobuf[128], count = 0, count0 = 0;
+	char s[12];
+
+	fifo32_init(&fifo, 128, fifobuf, 0);
+	timer_1s = timer_alloc();
+	timer_init(timer_1s, &fifo, 100);
+	timer_settime(timer_1s, 100);
+
+	for (; ; ){
+		count++;
+		io_cli();
+		if (fifo32_status(&fifo) == 0){
+			io_sti();
+		} else {
+			i = fifo32_get(&fifo);
+			io_sti();
+			if ( i == 100){
+				sprintf(s, "%11d", count- count0);
+				putfonts8_asc_sht(sht_win_b, 24, 28, COL8_000000, COL8_C6C6C6, s, 11);
+				count0 = count;
+				timer_settime(timer_1s, 100);
+			}
+		}
+	}
+}
+
+//命令行窗口
+void console_task(struct SHEET *sheet){
+	struct FIFO32 fifo;
+	struct TIMER *timer;
+	struct TASK *task = task_now();
+	
+	int i, fifobuf[128], cursor_x = 8, cursor_c = COL8_000000;
+	fifo32_init(&fifo, 128, fifobuf, task);
+	
+	timer = timer_alloc();
+	timer_init(timer, &fifo, 1);
+	timer_settime(timer, 50);
+	for (; ; ){
+		io_cli();
+		if (fifo32_status(&fifo) == 0){
+			task_sleep(task);
+			io_sti();
+		} else {
+			i = fifo32_get(&fifo);
+			io_sti();
+			//光标用定时器
+			if (i <= 1){
+				if (i != 0){
+					timer_init(timer, &fifo, 0);
+					cursor_c = COL8_FFFFFF;
+				} else {
+					timer_init(timer, &fifo, 1);
+					cursor_c = COL8_000000;
+				}
+				timer_settime(timer, 50);
+				boxfill8(sheet->buf, sheet->bxsize, cursor_c, cursor_x, 28, cursor_x + 7, 43);
+				sheet_refresh(sheet, cursor_x, 28, cursor_x + 8, 44);
+			}
+		}
+	}
+}
+
+//光标用定时器
+void gb_timer(struct SHEET *sheet, struct TIMER *timer, struct FIFO32 fifo){
+	int i, cursor_x = 8, cursor_c = COL8_000000;
+	i = fifo32_get(&fifo);
+	io_sti();
+	if (i <= 1){
+		if (i != 0){
+			timer_init(timer, &fifo, 0);
+			cursor_c = COL8_FFFFFF;
+		} else {
+			timer_init(timer, &fifo, 1);
+			cursor_c = COL8_000000;
+		}
+		timer_settime(timer, 50);
+		boxfill8(sheet->buf, sheet->bxsize, cursor_c, cursor_x, 28, cursor_x + 7, 43);
+		sheet_refresh(sheet, cursor_x, 28, cursor_x + 8, 44);
+	}
+}
---
+++ fakeOs17/helloos17_me/sheet.c
@@ -0,0 +1,292 @@
+//图层管理
+
+#include "bootpack.h"
+
+//初始化图层管理
+struct SHTCTL *shtctl_init(struct MEMMAN *memman, unsigned char *vram, int xsize, int ysize)
+{
+	struct SHTCTL *ctl;
+	int i;
+	//sizeof(类名) 可以自动计算出所需对象的字节数量(内存大小)
+	ctl = (struct SHTCTL *)memman_alloc_4k(memman, sizeof(struct SHTCTL));
+	if (ctl == 0)
+	{
+		goto err;
+	}
+
+	//
+	ctl->map = (unsigned char *)memman_alloc_4k(memman, xsize * ysize);
+	if (ctl->map == 0)
+	{
+		memman_free_4k(memman, (int)ctl, sizeof(struct SHTCTL));
+		goto err;
+	}
+
+	ctl->vram = vram;
+	ctl->xsize = xsize;
+	ctl->ysize = ysize;
+	//一个sheet都没有  
+	ctl->top = -1;
+	for (i = 0; i < MAX_SHEETS; i++)
+	{
+		//标记为未使用
+		ctl->sheets0[i].flags = 0;
+		//记录所属
+		ctl->sheets0[i].ctl = ctl;
+	}
+
+	err:
+		return ctl;
+}
+
+#define SHEET_USE			1
+//获取新生成的未使用图层
+struct  SHEET *sheet_alloc(struct SHTCTL *ctl)
+{
+	struct SHEET *sht;
+	int i;
+	//遍历所有的图层对象
+	for (i = 0; i < MAX_SHEETS; i++)
+	{
+		//判断是否已使用
+		if (ctl->sheets0[i].flags == 0)
+		{
+			//获取该未使用图层的地址
+			sht = &ctl->sheets0[i];
+			//标记为已使用
+			sht->flags = SHEET_USE;
+			//隐藏图层
+			sht->height = -1;
+			return sht;
+		}
+	}
+	///所有图层都在使用中
+	return 0;
+}
+
+//设定图层的缓冲区大小和透明色
+void sheet_setbuf(struct SHEET *sht, unsigned char *buf, int xsize, int ysize, int col_inv)
+{
+	//缓冲区
+	sht->buf = buf;
+	//大小
+	sht->bxsize = xsize;
+	sht->bysize = ysize;
+	//透明色
+	sht->col_inv = col_inv;
+	return;
+}
+
+//设定底板高度
+void sheet_updown(struct SHEET *sht, int height)
+{
+	struct SHTCTL *ctl = sht->ctl;
+	//存储设置前的高度
+	int h, old = sht->height;
+	//如果指定的高度过高或者过低，则进行修正
+	if (height > ctl->top+1)
+	{
+		height = ctl->top + 1;
+	}
+	if (height < -1)
+	{
+		height = -1;
+	}
+	//设定高度
+	sht->height = height;
+
+	//由于高度的变化需要对所有的图层进行重新排列
+	//比以前低
+	if (old > height)
+	{
+		//如果不是隐藏
+		if (height > 0)
+		{
+			//把中间的往上提
+			for (h = old; h < height; h--)
+			{
+				ctl->sheets[h] = ctl->sheets[h-1];
+				ctl->sheets[h]->height = h;
+			}
+			ctl->sheets[height] = sht;
+			//
+			sheet_refreshmap(ctl, sht->vx0, sht->vy0, sht->vx0 + sht->bxsize, sht->vy0 + sht->bysize, height + 1);
+			sheet_refreshsub(ctl, sht->vx0, sht->vy0, sht->vx0 + sht->bxsize, sht->vy0 + sht->bysize, height + 1, old);
+		}else{
+			//隐藏
+			if (ctl->top > old)
+			{
+				//把上面的降下来
+				for (h = old; h < ctl->top; h++)
+				{
+					ctl->sheets[h] = ctl->sheets[h+1];
+					ctl->sheets[h]->height = h;
+				}
+			}
+			//由于图层减少 所以最上面的图层高度下降
+			ctl->top--;
+			sheet_refreshmap(ctl, sht->vx0, sht->vy0, sht->vx0 + sht->bxsize, sht->vy0 + sht->bysize, 0);
+			sheet_refreshsub(ctl, sht->vx0, sht->vy0, sht->vx0 + sht->bxsize, sht->vy0 + sht->bysize, 0, old - 1);
+		}
+	}else if (old < height)//比以前高
+	{
+		if (old >= 0)
+		{
+			////把中间的拉下来
+			for (h = old; h < height; h++)
+			{
+				ctl->sheets[h] = ctl->sheets[h+1];
+				ctl->sheets[h] = h;
+			}
+			ctl->sheets[height] = sht;
+		}else{//由隐藏转为显示状态
+			//将已经在上面的提上来
+			for (h = ctl->top; h >= height; h--)
+			{
+				ctl->sheets[h+1] = ctl->sheets[h];
+				ctl->sheets[h+1]->height = h+1; 
+			}
+			ctl->sheets[height] = sht;
+			//由于已显示的图层新增一个，故最上面的图层高度增加
+			ctl->top++;
+		}
+	}
+	sheet_refreshmap(ctl, sht->vx0, sht->vy0, sht->vx0 + sht->bxsize, sht->vy0 + sht->bysize, height);
+	//按新图层重新绘制画面
+	sheet_refreshsub(ctl, sht->vx0, sht->vy0, sht->vx0 + sht->bxsize, sht->vy0 + sht->bysize, height, height);
+	return;
+}
+
+//刷新画面
+void sheet_refresh(struct SHEET *sht, int bx0, int by0, int bx1, int by1)
+{	
+	//如果正在显示，则按新图层刷新画面
+	if (sht->height >= 0)
+	{
+		sheet_refreshsub(sht->ctl, sht->vx0 + bx0, sht->vy0 + by0, sht->vx0 + bx1, sht->vy0 + by1, sht->height, sht->height);
+	}
+	return;
+}
+
+//局部刷新
+void sheet_refreshsub(struct SHTCTL *ctl, int vx0, int vy0, int vx1, int vy1, int h0, int h1)
+{
+	int h, bx, by, vx, vy, bx0, by0, bx1, by1;
+	unsigned char *buf,/* c,*/ *vram = ctl->vram, *map = ctl->map, sid;
+	struct SHEET *sht;
+
+	//判断refresh的范围是否超出画面
+	if (vx0 < 0) { vx0 = 0; }
+	if (vy0 < 0) { vy0 = 0; }
+	if (vx1 > ctl->xsize) { vx1 = ctl->xsize; }
+	if (vy1 > ctl->ysize) { vy1 = ctl->ysize; }
+
+	for (h = h0; h <= h1; h++)
+	{
+		sht = ctl->sheets[h];
+		buf = sht->buf;
+
+		sid = sht - ctl->sheets0;
+
+		//使用 vx0 - vy1, 对 bx0-by1 进行倒推
+		bx0 = vx0 - sht->vx0;
+		by0 = vy0 - sht->vy0;
+		bx1 = vx1 - sht->vx0;
+		by1 = vy1 - sht->vy0;
+
+		if (bx0 < 0){ bx0 = 0; }
+		if (by0 < 0){ by0 = 0; }
+		if (bx1 > sht->bxsize){ bx1 = sht->bxsize; }
+		if (by1 > sht->bysize){ by1 = sht->bysize; }
+
+		for (by = by0; by < by1; by++)
+		{
+			vy = sht->vy0 + by;
+			for (bx = bx0; bx < bx1; bx++)
+			{
+				vx = sht->vx0 + bx;
+				if (map[vy * ctl->xsize + vx] == sid)
+				{
+					vram[vy * ctl->xsize + vx] = buf[by * sht->bxsize + bx];
+				}
+			}
+		}
+	}
+	return;
+}
+
+//左右移动图层
+void sheet_slide(struct SHEET *sht, int vx0, int vy0)
+{
+	struct SHTCTL *ctl = sht->ctl;
+	int old_vx0 = sht->vx0, old_vy0 = sht->vy0;
+	sht->vx0 = vx0;
+	sht->vy0 = vy0;
+	//如果此图层正在显示则即刻刷新
+	if (sht->height >= 0)
+	{
+		sheet_refreshmap(ctl, old_vx0, old_vy0, old_vx0 + sht->bxsize, old_vy0 + sht->bysize, 0);
+		sheet_refreshmap(ctl, vx0, vy0, vx0 + sht->bxsize, vy0 + sht->bysize, sht->height);
+		sheet_refreshsub(ctl, old_vx0, old_vy0, old_vx0 + sht->bxsize, old_vy0 + sht->bysize, 0, sht->height -1);
+		sheet_refreshsub(ctl, vx0, vy0, vx0 + sht->bxsize, vy0 + sht->bysize, sht->height, sht->height);
+	}
+	return;
+}
+
+//释放已使用的图层内存函数
+void sheet_free(struct SHEET *sht)
+{
+	//如果还在显示状态 则设为隐藏
+	if (sht->height >= 0)
+	{
+		sheet_updown(sht, -1);
+	}
+	//标记为未使用状态
+	sht->flags = 0;
+	return;
+}
+
+//将图层置于map的函数
+void sheet_refreshmap(struct SHTCTL *ctl, int vx0, int vy0, int vx1, int vy1, int h0)
+{
+	int h, bx, by, vx, vy, bx0, by0, bx1, by1;
+	unsigned char *buf, sid, *map = ctl->map;
+	struct SHEET *sht;
+
+	if(vx0 < 0){ vx0 = 0; }
+	if(vy0 < 0){ vy0 = 0; }
+	if(vx1 > ctl->xsize){ vx1 = ctl->xsize; }
+	if(vy1 > ctl->ysize){ vy1 = ctl->ysize; }
+
+	for (h = h0; h<= ctl->top; h++)
+	{
+		sht = ctl->sheets[h];
+		//将进行剑法计算的地址作为图层号码使用
+		sid = sht - ctl->sheets0;
+		buf = sht->buf;
+		bx0 = vx0 - sht->vx0;
+		by0 = vy0 - sht->vy0;
+		bx1 = vx1 - sht->vx0;
+		by1 = vy1 - sht->vy0;
+
+		if(bx0 < 0){ bx0 = 0; }
+		if(by0 < 0){ by0 = 0; }
+		if(bx1 > sht->bxsize){ bx1 = sht->bxsize; }
+		if(by1 > sht->bysize){ by1 = sht->bysize; }
+
+		for (by = by0; by < by1; by++)
+		{
+			vy = sht->vy0 + by;
+
+			for (bx = bx0; bx < bx1; bx++)
+			{
+				vx = sht->vx0 + bx;
+				if (buf[by * sht->bxsize + bx] != sht->col_inv)
+				{
+					map[vy * ctl->xsize + vx] = sid;
+				}
+			}
+		}
+	}
+	return;
+}
